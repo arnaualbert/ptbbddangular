b5027440102ec370148c077eddd9eb56b37f1ec2
arnaualbert <arnaualbert2003@gmail.com>
2023-02-28 11:44:40 +0000

    comments

--- src/app/services/deleteanimalservice.service.ts
+++ src/app/services/deleteanimalservice.service.ts
@@ -6,9 +6,24 @@ import { Animal } from '../model/Animal';
 @Injectable({
   providedIn: 'root'
 })
+/**
+ * @description delete a specific animal from the database
+ */
 export class DeleteanimalserviceService {
+  /**
+   * @description declaration of variables
+   */
   url:string= 'http://localhost:3000'
+  /**
+   * Make the controller with the services we need
+   * @param _http 
+   */
   constructor(private _http:HttpClient) { }
+  /**
+   * delete a specific animal from the database
+   * @param nombre is the name of the animal to delete
+   * @returns the response of the server
+   */
   deleteanimal(nombre:any){
     return this._http.post(this.url+'/deleteanimal',{'nombre':nombre},{responseType:"json"})
   }
